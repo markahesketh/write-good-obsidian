9c3e3cb88f8e3598cf4669cbb5697ec165c4e9e2
Mark Hesketh <contact@markhesketh.co.uk>
2025-02-14 22:49:08 +0000

    Initial implementation

--- main.ts
+++ main.ts
@@ -1,134 +1,72 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import writeGood from 'write-good';
+import { Decoration, ViewPlugin, WidgetType } from '@codemirror/view';
 
-// Remember to rename these classes and interfaces!
+class WriteGoodWidget extends WidgetType {
+  constructor(readonly message: string) {
+    super();
+  }
 
-interface WriteGoodSettings {
-	mySetting: string;
+  toDOM() {
+    const div = document.createElement('div');
+    div.className = 'write-good-suggestion';
+    div.textContent = this.message;
+    return div;
+  }
 }
 
-const DEFAULT_SETTINGS: WriteGoodSettings = {
-	mySetting: 'default'
-}
+const writeGoodPlugin = ViewPlugin.fromClass(
+  class {
+    constructor(view) {
+      this.view = view;
+      this.decorations = this.buildDecorations();
+      console.log('WriteGoodPlugin initialized');
+    }
 
-export default class WriteGood extends Plugin {
-	settings: WriteGoodSettings;
+    update(update) {
+      if (update.docChanged || update.selectionSet) {
+        this.decorations = this.buildDecorations();
+        console.log('Document updated');
+      }
+    }
 
-	async onload() {
-		await this.loadSettings();
+    buildDecorations() {
+      const builder = [];
+      const doc = this.view.state.doc;
+      const text = doc.toString();
+      const suggestions = writeGood(text);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+      console.log('Evaluating text:', text);
+      console.log('Suggestions:', suggestions);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+      for (const suggestion of suggestions) {
+        const from = suggestion.index;
+        const to = suggestion.index + suggestion.offset;
+        const mark = Decoration.mark({
+          class: 'write-good-highlight',
+        });
+        builder.push(mark.range(from, to));
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        const widget = Decoration.widget({
+          widget: new WriteGoodWidget(suggestion.reason),
+          side: 1,
+        });
+        builder.push(widget.range(doc.lineAt(from).to));
+      }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+      return Decoration.set(builder);
+    }
+  },
+  {
+    decorations: (v) => v.decorations,
+  }
+);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+export default class WriteGoodPlugin extends Plugin {
+  async onload() {
+    this.registerEditorExtension(writeGoodPlugin);
+    console.log('WriteGoodPlugin loaded');
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: WriteGood;
-
-	constructor(app: App, plugin: WriteGood) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  onunload() {}
 }
